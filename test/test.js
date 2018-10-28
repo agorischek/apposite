a996c247c2dddd7157bb593c7496ee53b46c5995
agorischek <alex.gorischek@gmail.com>
2018-10-28 00:01:01 -0700

    Add tests

--- test/test.js
+++ test/test.js
@@ -8,4 +8,13 @@ describe("Apposite", function(){
     it("should pass through a general section", function(){
         assert.equal(apposite.render("@@@ * @@@\nABC"), "ABC\n");
     });
+    it("should ignore a null target", function(){
+        assert.equal(apposite.render("@@@ * @@@\nABC", null), "ABC\n");
+    });
+    it("should render for a target", function(){
+        assert.equal(apposite.render("@@@ * @@@\nABC\n@@@ a @@@\nDEF\n@@@ b @@@\nGHI", "a"), "ABC\nDEF\n");
+    });
+    it("should accept a custom marker", function(){
+        assert.equal(apposite.render("&& * &&\nABC", null, "&&"), "ABC\n");
+    });
 });
